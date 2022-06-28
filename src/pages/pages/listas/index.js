803c7b0c7355169a48f2d330830373e016b79bac
gokrusk <nigelljama@gmail.com>
2022-06-28 12:05:11 -0500

    cleaning code

--- src/pages/pages/listas/index.js
+++ src/pages/pages/listas/index.js
@@ -4,12 +4,6 @@ import Grid from '@mui/material/Grid'
 // ** Styled Component
 import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
 
-// ** Demo Components Imports
-import FormLayoutsBasic from 'src/views/form-layouts/FormLayoutsBasic'
-import FormLayoutsIcons from 'src/views/form-layouts/FormLayoutsIcons'
-import FormLayoutsSeparator from 'src/views/form-layouts/FormLayoutsSeparator'
-import FormLayoutsAlignment from 'src/views/form-layouts/FormLayoutsAlignment'
-
 // ** Third Party Styles Imports
 import 'react-datepicker/dist/react-datepicker.css'
 
